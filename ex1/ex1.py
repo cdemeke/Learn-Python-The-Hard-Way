396dbe68c0f85972ccce9ff42d61e2aaf17dbd49
cdemeke <demeke2@gmail.com>
2014-10-20 21:28:30 -0400

    Learn Python the Hard Way
    Documenting my lpthw exercises

---
+++ ex1/ex1.py
@@ -0,0 +1,11 @@
+print "Hello World!"
+print "Hello Again!"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather not 'not'."
+print 'I "said" do not touch this.'
+
+#study drill
+print "another line example"
+print "#what?"
